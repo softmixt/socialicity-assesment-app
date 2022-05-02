c5e19ba0221adf993088a9a87a617568a812c048
Baghina Radu Adrian <softmixt@gmail.com>
2022-05-02 21:47:42 +0200

    Setting up basic ag-grid component

---
+++ src/pages/customer/users/UsersDetailsViewPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function UsersDetailsViewPage() {
+  return (<h1>Users Details View Page</h1>);
+}
--- src/parts/users/ListUsers.tsx
+++ src/parts/users/ListUsers.tsx
@@ -1,6 +1,11 @@
 import React, {
-  useEffect,
+  useCallback,
+  useEffect, useMemo, useRef, useState,
 } from 'react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+import { AgGridReact } from 'ag-grid-react';
 
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import {
@@ -12,16 +17,83 @@ import { toast } from 'react-hot-toast';
 
 export default function ListUsers() {
   const dispatch = useAppDispatch();
+  const gridRef = useRef<any>();
 
+  // States ...
+  const [rowData, setRowData] = useState<any>();
+
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      field: 'id',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'email',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'first_name',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'last_name',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'avatar',
+      filter: false,
+      sortable: false,
+    },
+  ]);
+
+  // Selectors ...
   const users = useAppSelector(selectUsers);
-  const isUsersLoading: boolean = useAppSelector<boolean>(isLoading);
 
-  useEffect(() => {
-    dispatch(fetchUsersDataAsync())
-      .catch(() => toast.error(''));
+  const containerStyle = useMemo(() => ({
+    height: '25rem',
+    width: '100%',
+  }), []);
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter: true,
+    resizable: true,
+    floatingFilter: true,
+  }), []);
+
+  const onGridReady = useCallback((params) => {
+    if (!users.length) {
+      dispatch(fetchUsersDataAsync()).catch(() => toast.error(''));
+    }
   }, []);
 
+  const onFirstDataRendered = useCallback((params) => {
+    gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+  useEffect(() => {
+    setRowData(users);
+  }, [users]);
+
   return (
-    <h1>Welcome</h1>
+    <div
+      className="container-fluid ag-theme-alpine pb-3"
+      style={containerStyle}
+    >
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        rowDragManaged
+        rowDragMultiRow
+        defaultColDef={defaultColDef}
+        animateRows
+        pagination
+        paginationPageSize={5}
+        paginationAutoPageSize
+        onGridReady={onGridReady}
+        onFirstDataRendered={onFirstDataRendered}
+      />
+    </div>
   );
 }
