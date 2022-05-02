bbcd39fd062c983cb4316ec6489564573a2f4651
Baghina Radu Adrian <softmixt@gmail.com>
2022-05-03 01:33:30 +0200

    Updates

--- static/js/899.04e23705.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksocialicity_assesment_app=self.webpackChunksocialicity_assesment_app||[]).push([[899],{3899:function(s,e,n){n.r(e),n.d(e,{default:function(){return i}});n(2791);var c=n(184);function i(s){return(0,c.jsx)("div",{className:"row",children:(0,c.jsx)("h1",{children:"Users New"})})}}}]);
-//# sourceMappingURL=899.04e23705.chunk.js.map
